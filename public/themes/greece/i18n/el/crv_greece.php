5e118663cb8788d0db355bc1b28db61e310b2db7
John Etherton <john.etherton@gmail.com>
2013-04-05 00:34:20 -0600

    added anonymity policy to the greek translation

--- public/themes/greece/i18n/el/crv_greece.php
+++ public/themes/greece/i18n/el/crv_greece.php
@@ -2,6 +2,6 @@
 $lang = array(
 	'tag_line' => 'Crowdsourcing Racist Violence',
 	'a_project_of'=>'Μία έρευνα του πανεπιστημίου',
-	'anonymity_policy'=>'Anonymity Policy',
+	'anonymity_policy'=>'Πολιτική ανωνυμίας',
 		);
 ?>
