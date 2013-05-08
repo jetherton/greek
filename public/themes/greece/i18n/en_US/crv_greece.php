9be4f85a3de37ae878f22a85739152b8aa52416b
John Etherton <john.etherton@gmail.com>
2013-05-08 12:21:07 -0600

    updated the language files

--- public/themes/greece/i18n/en_US/crv_greece.php
+++ public/themes/greece/i18n/en_US/crv_greece.php
@@ -1,9 +1,9 @@
 <?php
 $lang = array(
-	'tag_line' => 'Crowdsourcing Racist Violence',
-	'a_project_of'=>'A project of',
-	'anonymity_policy'=>'Anonymity Policy',
-	'ip_address'=>'IP Address',
+		'tag_line' => 'Crowdsourcing Racist Violence',
+		'a_project_of'=>'A project of',
+		'anonymity_policy'=>'Anonymity Policy',
+		'ip_address'=>'IP Address',
 		'submitted_by'=>'Submitted by',
 		'reports_from_this_ip'=>'Reports from this IP',
 		'reports_from_this_user'=>'Reports from this user',
